05acf16dca8dc379944399720bbf4cf85cd0f0ba
Toh <jagraphob@axos.systems>
2025-01-22 17:46:02 +1300

    finish lesson 4

--- lib/tutorial_web/live/cookies_live.ex
+++ lib/tutorial_web/live/cookies_live.ex
@@ -5,8 +5,10 @@ defmodule TutorialWeb.CookiesLive do
     ~H"""
     <div class="min-h-screen flex flex-col items-center justify-center bg-gradient-to-b from-purple-900 to-red-900 text-white">
       <h1 class="absolute top-5 left-5 text-2xl font-bold">Cookie Clicker</h1>
+      <div class="text-6xl mb-4">{@count}</div>
       <div
         class="text-9xl cursor-pointer select-none transition-transform duration-100 hover:scale-110 active:scale-90"
+        phx-click="increment"
       >
         🍪
       </div>
@@ -16,5 +18,10 @@ defmodule TutorialWeb.CookiesLive do
 
   def mount(_params, _session, socket) do
     {:ok, assign(socket, count: 0)}
+  end
+
+  def handle_event("increment", _parms, socket) do
+    new_count = socket.assigns.count + 1
+    {:noreply, assign(socket, :count, new_count)}
   end
 end
